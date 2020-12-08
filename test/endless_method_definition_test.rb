aa8b8abe0d54f677458bffd15b2938c2884544b0
Junichi Ito <jit@sonicgarden.jp>
2020-12-08 19:06:08 +0900

    Add tests for endless method

--- test/endless_method_definition_test.rb
+++ test/endless_method_definition_test.rb
@@ -15,4 +15,28 @@ class EndlessMethodDefinitionTest < Minitest::Test
       def age=(n) = @age = n
     RUBY
   end
+
+  # Modified from https://github.com/ruby/ruby/blob/v3_0_0_preview2/test/ruby/test_syntax.rb#L1426-L1445
+  def f1 = 42
+  def f2() = 42
+  def f3(x) = x + 1
+  def f4(x) = 1 / x rescue nil
+  def f5(x)
+    =
+    x +
+    1
+  def f6(x) = @foo = x
+
+  def test_others
+    assert_equal 42, f1
+    assert_equal 42, f2
+    assert_equal 2, f3(1)
+    assert_nil f4(0)
+    assert_equal 2, f5(1)
+    assert_equal 1, f6(1)
+    assert_equal 1, @foo
+    refute_syntax(<<~RUBY)
+      def f7 x = x + 1
+    RUBY
+  end
 end
